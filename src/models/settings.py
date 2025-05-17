e9006eeb89d435c857997d78f90df07ad0a10dca
123botieno <benadetteotieno@gmail.com>
2025-05-17 14:03:10 +0300

    moved aws secrets to env file

---
+++ src/models/settings.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    AWS_ACCESS_KEY_ID : str
+    AWS_SECRET_ACCESS_KEY : str
+    AWS_DEFAULT_REGION : str
+
+    class Config:
+        env_file = './.env'
+
+settings = Settings()
