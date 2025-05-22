03010aabf7b1b71e683c09c809f7e160a4b5d0eb
EC2 Default User <ec2-user@ip-172-31-81-82.ec2.internal>
2025-05-22 14:47:04 +0000

    adding refresh model function

---
+++ src/training/refresh_model.py
@@ -0,0 +1,130 @@
+import xgboost as xgb
+from sklearn.preprocessing import OneHotEncoder
+from datetime import datetime
+import random
+import boto3
+import io
+import pandas as pd
+import pickle
+import shutil
+
+
+def refresh_model(pipeline = False, targets_df = None, targets_aws = None, refresh_date = str):
+
+    # first, read in the processed dataset
+    # if pipeline, then dataset is in the pipeline
+    # else, it is in the AWS S3 bucket
+    if pipeline:    
+        df = targets_df
+    else:
+        # Define S3 info
+        bucket = 'kehmisjan2025'
+        # Initialize boto3 client
+        s3 = boto3.client('s3')
+        buffer = io.BytesIO()
+        s3.download_fileobj(bucket, targets_aws, buffer)
+        buffer.seek(0)
+        df = pd.read_parquet(buffer)
+
+    # make sure nad is a datetime
+    df['nad'] = pd.to_datetime(df['nad'], format='%Y-%m-%d')
+
+    # filter to refresh period
+    refresh_date = pd.Timestamp(refresh_date)
+
+    # Filter to records from the refresh date and six months before
+    # Define the date range to exclude
+    after = refresh_date - pd.DateOffset(months=6)
+    before = refresh_date
+    df = df[(df['nad'] >= after) & (df['nad'] <= before)]
+
+    # filter out where nad imputation flag is 1
+    df = df[df['nad_imputation_flag'] == 0]
+
+    df = df.drop(columns=[
+        'key', 'visitdate', 'nad_imputation_flag', 'sitecode', 'pregnant_missing',
+        'breastfeeding_missing', 'startartdate', 'month', 'dayofweek', 'timeatfacility',
+        'code', 'county'])
+
+    # filter to emr in kenyamer and ecare   
+    df = df[df['emr'].isin(['kenyaemr', 'ecare'])]
+    # Emr: KenyaEMR -> 1, else 0
+    df['emr'] = (df['emr'] == 'kenyaemr').astype('Int64') 
+
+    # convert whostage and adherence to integers
+    df['whostage'] = df['whostage'].astype('float').astype('Int64')
+    df['adherence'] = df['adherence'].astype('Int64')
+
+    categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+
+    ohe = OneHotEncoder(drop='first', handle_unknown='ignore')
+    ohe.fit(df[categorical_columns])
+
+    # Save the fitted encoder
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    with open(f"models/ohe_{timestamp}.pkl", "wb") as f:
+        pickle.dump(ohe, f)
+    # Save the refreshed encoder as latest to be used in inference
+    shutil.copyfile(f"models/ohe_{timestamp}.pkl", "models/ohe_latest.pkl")
+
+
+    def encode_xgboost(df, start_date, end_date):
+
+        # Filter the DataFrame to include only the rows within the specified date range
+        df = df[(df['nad'] >= start_date) & (df['nad'] <= end_date)]
+        # Drop the 'nad' column from the DataFrame
+        df = df.drop(columns=['nad'])
+
+        # One-hot encode the categorical columns
+        encoded_features = ohe.transform(df[categorical_columns]).toarray()
+        encoded_feature_names = ohe.get_feature_names_out(categorical_columns)
+        
+        # Create a DataFrame with the encoded features
+        encoded_df = pd.DataFrame(encoded_features, columns=encoded_feature_names, index=df.index)
+        
+        # Concatenate the encoded features with the original DataFrame
+        final_df = pd.concat([df.drop(columns=categorical_columns), encoded_df], axis=1)
+
+        # convert to xgb.Dmatrix
+        xgb_df = xgb.DMatrix(
+            data=final_df.drop(columns=["iit"]),
+            label=final_df["iit"]
+        )
+    
+        return xgb_df
+
+        
+    # encoded dataset
+    dtrain = encode_xgboost(df, start_date = after, end_date = refresh_date - pd.DateOffset(months=1))
+    dval = encode_xgboost(df, start_date = refresh_date - pd.DateOffset(months=1), end_date = refresh_date)
+
+    params = {
+        'eta': 0.01,
+        'max_depth': 6,
+        'subsample': 0.5,
+        'colsample_bytree': 0.6,
+        'lambda': 1,
+        'scale_pos_weight': 10,
+        'objective': 'binary:logistic',
+        'eval_metric': 'aucpr',
+    }
+
+    random.seed(42)
+    gb_model = xgb.train(
+        params=params,
+        dtrain=dtrain,
+        num_boost_round=3000,
+        evals=[(dtrain, 'train'), (dval, 'eval')],
+        early_stopping_rounds=100,
+        verbose_eval=False
+    )
+
+    # Save the refreshed model with time stamp
+    with open(f"models/mod_{timestamp}.pkl", "wb") as f:
+        pickle.dump(gb_model, f)
+    # Save the refreshed model as latest to be used in inference
+    shutil.copyfile(f"models/mod_{timestamp}.pkl", "models/mod_latest.pkl")
+
+
+
+
