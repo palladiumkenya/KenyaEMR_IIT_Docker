847d78ea6e9ffa11b0583953a805dc002de3af67
EC2 Default User <ec2-user@ip-172-31-81-82.ec2.internal>
2025-05-22 14:47:26 +0000

    adding inference pipeline

---
+++ pipelines/inference_pipeline.py
@@ -0,0 +1,38 @@
+# Local application imports
+from src.common import get_data
+from src.common import clean_data
+from src.common import visit_features
+from src.common import dem_features
+from src.common import create_target
+from src.common import target_features
+from src.inference import locational_features_inf
+from src.inference import generate_inference
+
+# general imports
+import boto3
+import io
+import pandas as pd
+import time
+s3 = boto3.client('s3')
+
+def run_inference_pipeline(aws = False, prediction = True, ppk = str, sc = str, start_date = str, end_date = str):
+
+    # For retraining, prediction is False, so won't add that as argument to parent function
+    lab, pharmacy, visits, dem, mfl, dhs, txcurr = get_data.get_data(aws = aws, 
+                                                                     prediction = prediction,
+                                                                     patientPK= ppk,
+                                                                     sitecode= sc)
+
+    # Run cleaning and feature preparation functions
+    lab = clean_data.clean_lab(lab, start_date = start_date)
+    pharmacy = clean_data.clean_pharmacy(pharmacy, start_date = start_date, end_date = end_date)
+    visits = clean_data.clean_visits(visits, dem, start_date = start_date, end_date = end_date)
+    visits = visit_features.prep_visit_features(visits)
+    visits = dem_features.prep_demographics(visits)
+    targets = create_target.create_target(visits, pharmacy, dem)
+    targets = target_features.prep_target_visit_features(targets, visits)
+    targets = target_features.prep_target_pharmacy_features(targets, pharmacy)
+    targets = target_features.prep_target_lab_features(targets, lab)
+    targets = locational_features_inf.get_locational_features(targets)
+    pred = generate_inference.gen_inference(targets)
+    return pred
